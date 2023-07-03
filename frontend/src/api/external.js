5c71246ce7b8c2b3f4f4d737d0d772880e8b112b
umairfarooq <umair.codedeveloper@gmail.com>
2023-07-03 20:04:51 +0500

    befour Home page

---
+++ frontend/src/api/external.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+
+const NEWS_API_ENDPOINT =``
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { useState,useEffect } from "react"
+
+function Home(){
+    const [articals,setArticals ] =useState([]);
+
+    return (
+        <p>home from pages is it working oooo yes</p>
+    )
+}
+export default Home
